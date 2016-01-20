fbdae72752202b2e449786f1f2e3e72e84453e30
MaitrayeeGhosh <maitrayeeghosh89@gmail.com>
2016-01-20 07:56:36 +0530

    Initial Commit

---
+++ TableViewEditModeExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewEditModeExample
+//
+//  Created by Maitrayee Ghosh on 20/01/16.
+//  Copyright © 2016 Maitrayee Ghosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
